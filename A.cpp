6bf032176e07f2c8477f1d65fd00f51a1ed7a9f8
Durjoy1971 <91456847+Durjoy1971@users.noreply.github.com>
2023-01-13 16:19:06 +0600

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,62 @@
+/*  -------------------------      Namo Buddhaya        ------------------------   */
+
+/*
+
+    It's Only Pain Which Lead's Us To Achieve Some Thing Special.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(i) cout << "Debug " << i << nl
+#define nl "\n"
+#define sp " "
+#define ll long long int
+#define ld long double
+#define PI 2 * acos(0.0)
+#define mem(arr, fix) memset(arr, fix, sizeof(arr))
+#define eps 1e-6
+#define mkp make_pair
+#define valid(nx, ny) (nx >= 1 && nx <= n) && (ny >= 1 && ny <= m)
+#define Dpos(n) fixed << setprecision(n)
+#define ff first
+#define ss second
+#define vi vector<int>
+#define vl vector<long long int>
+#define mod ((ll)1e9 + 7)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(v) v.begin(), v.end()
+#define allrev(v) v.rbegin(), v.rend()
+#define vin(name) \
+    ll num;       \
+    cin >> num;   \
+    name.push_back(num);
+
+// Global Variable && Functions
+
+// Main Codes Start Here
+void Solve()
+{
+    ll i, j;
+
+
+}
+
+// Main Function
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //:]
+    int t = 1;
+
+    cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Solve();
+    }
+}
